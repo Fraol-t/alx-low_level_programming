bd5214bedd1a31b852e9c00c73e0c6fc10a8b67a
Fraol-t <fraoltz5@gmail.com>
2023-09-08 02:24:07 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,18 +17,16 @@ int main(void)
     /* Generate a random number and store it in n */
     n = rand() - RAND_MAX / 2;
 
-    /* Calculate the last digit of n */
-    int last_digit = n % 10;
+    int last_digit = n % 10; // Calculate the last digit of n
 
-    /* Check if last_digit is greater than 5, equal to 0, or less than 6 and not 0, and print the result */
-    printf("Last digit of %d is %d ", n, last_digit);
+    printf("Last digit of %d is %d and is ", n, last_digit);
 
     if (last_digit > 5)
-        printf("and is greater than 5\n");
+        printf("greater than 5\n");
     else if (last_digit == 0)
-        printf("and is 0\n");
+        printf("0\n");
     else
-        printf("and is less than 6 and not 0\n");
+        printf("less than 6 and not 0\n");
 
     return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     char letter = 'a';
@@ -14,3 +19,4 @@ int main(void)
 
     return (0);
 }
+
