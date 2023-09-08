4b0fb5ad52c9650f5de3c934937ebb92fdf43999
Fraol-t <fraoltz5@gmail.com>
2023-09-08 02:16:05 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
     int n;
+
     /* Initialize random seed */
     srand(time(0));
+
     /* Generate a random number and store it in n */
     n = rand() - RAND_MAX / 2;
+
     /* Check if n is positive, negative, or zero, and print the result */
     if (n > 0)
         printf("%d is positive\n", n);
@@ -19,5 +24,6 @@ int main(void)
         printf("%d is negative\n", n);
     else
         printf("%d is zero\n", n);
+
     return (0);
 }
