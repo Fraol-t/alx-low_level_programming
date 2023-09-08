de893e62f0397d4a65325433c78aa8e37dcee6d7
Fraol-t <fraoltz5@gmail.com>
2023-09-08 04:09:56 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int digit;
+	int digit;
 
-    for (digit = 0; digit <= 9; digit++)
-    {
-        putchar(digit + '0');  // Convert the digit to its character representation
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0'); /* Convert the digit to its character representation */
 
-        if (digit < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+		if (digit != 9)
+		{
+			putchar(','); /* Print a comma after the digit */
+			putchar(' '); /* Print a space after the comma */
+		}
+	}
 
-    putchar('\n');
+	putchar('\n'); /* Print a newline character at the end */
 
-    return (0);
+	return (0);
 }
+
