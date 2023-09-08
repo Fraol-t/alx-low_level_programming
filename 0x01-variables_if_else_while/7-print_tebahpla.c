eb1ae5c8160700eb74be88b308dd4a97de485347
Fraol-t <fraoltz5@gmail.com>
2023-09-08 04:03:43 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse,
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char letter = 'z';
